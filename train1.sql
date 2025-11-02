d22bc46850736288a6277550d0fb879c557c086d
Abhi-205 <abhayjadhav205@gmail.com>
2025-11-02 11:59:10 +0530

    Update with JOIN

--- train1.sql
+++ train1.sql
@@ -3,17 +3,24 @@ use fymca;
 
 -- 1)List the information about the avilability of trains between Mumbai and pune
 
-SELECT *
-FROM train 
-where(lower(source_stn) = 'Mumbai' AND lower(dest_stn) = 'Pune')
-OR(lower(source_stn) = 'Pune' and lower(dest_stn) = 'Mumbai');
+-- SELECT *
+-- FROM train 
+-- where(lower(source_stn) = 'Mumbai' AND lower(dest_stn) = 'Pune')
+-- OR(lower(source_stn) = 'Pune' and lower(dest_stn) = 'Mumbai');
 
 -- 2)Gives the name of all trains which start from Pune
-SELECT tname as 'Train Name'
-FROM train 
-Where lower(source_stn) = 'pune' ;
+-- SELECT tname as 'Train Name'
+-- FROM train 
+-- Where lower(source_stn) = 'pune' ;
 
 -- 3)count number of passenger for 'Duronoto Express'
 SELECT count(*) as 'Total Passenger'
 FROM ticket
 where tno IN(SELECT tno from train where tname = 'Duronto');
+
+-- Use JOINS
+SELECT COUNT(*) as 'Total Passenger'
+FROM ticket as t
+JOIN train
+ON t.tno = train.tno 
+where train.tname = 'Duronto';
