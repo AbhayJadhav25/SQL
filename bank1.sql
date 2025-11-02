4e93cfa5e3b9db0eb2af5863562bf0473c2c4e4c
Abhi-205 <abhayjadhav205@gmail.com>
2025-11-02 11:43:37 +0530

    Use JOINS

--- bank1.sql
+++ bank1.sql
@@ -1,7 +1,7 @@
 -- Simple queries
 -- Find out customer name having loan amount > 100000
 use fymca;
-select cname from customer where cno IN(select cno from loan_appl where lamtrequired > 100000);
+-- select cname from customer where cno IN(select cno from loan_appl where lamtrequired > 100000);
 
 -- List the name of the customer who have loan at ______ branch
 select cname from customer where bid In (select bid from loan_appl where bid in(select bid from branch where brname = 'Kothrud' ));
@@ -9,3 +9,23 @@ select cname from customer where bid In (select bid from loan_appl where bid in(
 -- find out the total amount sanctioned by ____ branch;
 select sum(lamtapproved) as 'Approved ammount '  from loan_appl where bid in(select bid from branch where brname = "Kothrud");
 
+-- -- Using JOINS
+-- select c.cname as 'Customer name'
+-- from customer as c 
+-- LEFT JOIN loan_appl as l
+-- ON c.cno = l.cno
+-- where l.lamtrequired >= 100000;
+
+SELECT c.cname as 'Customer name'
+FROM customer as c
+JOIN loan_appl as l 
+ON c.cno = l.cno 
+JOIN branch as b 
+ON l.bid = b.bid
+where b.brname = 'Kothrud';
+
+SELECT SUM(lamtapproved) as 'Total Loan Amount Approved'
+FROM loan_appl as l
+JOIN branch as b
+ON l.bid = b.bid
+where b.brname = 'Kothrud';
