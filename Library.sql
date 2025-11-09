5f57b8dbc487e7895f1b2cf1a5387308a37f40d2
Abhi-205 <abhayjadhav205@gmail.com>
2025-11-09 23:06:24 +0530

    Trigger;

--- Library.sql
+++ Library.sql
@@ -1,57 +1,60 @@
 -- Database: Library Management System/
-drop table borrow;
-drop table members;
-drop table books;
+DROP TABLE IF EXISTS borrow;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS books;
 
-create table books(
-  book_id int primary key NOT NULL auto_increment, 
-  title varchar(50),
-  author varchar(50) ,
-  catrgory varchar(30) ,
-  price decimal(8,2),
-  avilable_copies int
-  );
-
-create table members(
-  member_id int primary key NOT NULL,
-  name varchar(30),
-  member_addr varchar(50) , 
-  phone varchar(10) ,
-  membership_date date
+-- BOOKS TABLE
+CREATE TABLE books (
+  book_id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(50),
+  author VARCHAR(50),
+  category VARCHAR(30),
+  price DECIMAL(8,2),
+  available_copies INT
 );
 
-create table borrow(
-  borrow_id int primary key not null ,
-  member_id int ,
-  foreign key (member_id) references members(member_id),
-  book_id int ,
-  foreign key (book_id) references books(book_id) ,
-  borrow_date date ,
-  return_date date ,
-  fine decimal(8,2)
+-- MEMBERS TABLE
+CREATE TABLE members (
+  member_id INT PRIMARY KEY,
+  name VARCHAR(30),
+  member_addr VARCHAR(50),
+  phone VARCHAR(10),
+  membership_date DATE
 );
-  
-insert into books Values
-(1,'Vede man' , 'Mr.om' , 'Love' , 1480 ,20),
-(2,'RA&W' , 'R.K. Narayan' , 'Real Story' , 1800 , 25),
-(3,'Marathi Poem','R.K. Narayan','Poems',1200 , 30) ,
-(4,'Hindi Stories' , 'A Shastri' , 'Funny' , 1500 , 4),
-(5,'English Horror' , 'R jadhav','Horror',2500,12);
 
+-- BORROW TABLE
+CREATE TABLE borrow (
+  borrow_id INT PRIMARY KEY,
+  member_id INT,
+  FOREIGN KEY (member_id) REFERENCES members(member_id),
+  book_id INT,
+  FOREIGN KEY (book_id) REFERENCES books(book_id),
+  borrow_date DATE,
+  return_date DATE,
+  fine DECIMAL(8,2)
+);
 
-insert into members Values
-(11 , 'Aditya' , 'Pune' , '91827179','2023-10-11'),
-(12 , 'Mayur' , 'Ahilyanagr' , '098768','2025-08-12'),
-(13,"Harshal" , 'Ravet','098767','2024-05-16'),
-(14,"kartik","Bhingar",'5654345','2022-10-26'),
-(15,"Abhishek",'Wagholi','1627291','2024-10-12');
+-- SAMPLE DATA
+INSERT INTO books VALUES
+(1,'Vede Man', 'Mr. Om', 'Love', 1480, 20),
+(2,'RA&W', 'R.K. Narayan', 'Real Story', 1800, 25),
+(3,'Marathi Poem', 'R.K. Narayan', 'Poems', 1200, 30),
+(4,'Hindi Stories', 'A. Shastri', 'Funny', 1500, 4),
+(5,'English Horror', 'R. Jadhav', 'Horror', 2500, 12);
 
-insert into borrow Values
-(21, 11 , 1 , '2023-08-12' , '2023-09-12',120),
-(22 , 12 , 2 ,'2025-04-21','2025-06-22',240),
-(23 , 13 , 2 , '2024-02-18','2024-04-19',150),
-(24 , 14 , 4 ,'2022-08-23','2022-09-21',1000),
-(25 , 14 , 5 , '2022-06-16','2022-08-13',127);
+INSERT INTO members VALUES
+(11, 'Aditya', 'Pune', '91827179', '2023-10-11'),
+(12, 'Mayur', 'Ahilyanagar', '098768', '2025-08-12'),
+(13, 'Harshal', 'Ravet', '098767', '2024-05-16'),
+(14, 'Kartik', 'Bhingar', '5654345', '2022-10-26'),
+(15, 'Abhishek', 'Wagholi', '1627291', '2024-10-12');
+
+INSERT INTO borrow VALUES
+(21, 11, 1, '2023-08-12', '2023-09-12', 120),
+(22, 12, 2, '2025-04-21', '2025-06-22', 240),
+(23, 13, 2, '2024-02-18', '2024-04-19', 150),
+(24, 14, 4, '2022-08-23', '2022-09-21', 1000),
+(25, 14, 5, '2022-06-16', '2022-08-13', 127);
 
 
 -- Queries
@@ -161,7 +164,7 @@ BEGIN
   RETURN delay_fine;
 END $$
 
-DELIMITER ;*/
+DELIMITER ;
 
 DROP FUNCTION IF EXISTS display_copies;
 DELIMITER $$
@@ -185,4 +188,36 @@ WHERE book_id = id;
 SET avilable = total_copies - borrowd_cnt;
 RETURN avilable;
 END $$
+DELIMITER ;*/
+
+DROP TRIGGER IF EXISTS  after_borrow;
+DELIMITER $$
+CREATE TRIGGER after_borrow 
+AFTER INSERT ON borrow
+FOR EACH ROW
+BEGIN
+UPDATE books 
+SET available_copies = available_copies  - 1
+WHERE book_id = NEW.book_id;
+END $$
 DELIMITER ;
+INSERT INTO borrow (borrow_id, member_id ,book_id, borrow_date, return_date)
+VALUES (26, 12, 5, '2025-11-09', NULL);
+
+DROP TRIGGER IF EXISTS  after_return;
+DELIMITER $$
+CREATE TRIGGER after_return
+AFTER UPDATE ON borrow
+FOR EACH ROW
+BEGIN
+IF NEW.return_date IS NOT NULL AND OLD.return_date IS NULL THEN
+UPDATE books
+SET  available_copies = available_copies + 1
+WHERE book_id = NEW.book_id ;
+END IF;
+END $$
+DELIMITER ;
+
+UPDATE borrow
+SET return_date = '2025-11-15'
+WHERE borrow_id = 26;
